97ba97e75248547ab03dc0a518ab40042f6f3be0
SarahFdes <fdes.sarah@gmail.com>
2025-03-06 10:09:36 +0530

    hero component added

---
+++ src/components/Hero.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import '../styles.css';
+
+const Hero = () => {
+  return (
+    <section className="hero section-padding">
+      <div className="hero-container">
+        {/* First Column */}
+        <div className="hero-text">
+          <h1 className="hero-title">Little Lemon</h1>
+          <h2 className="hero-subheading">Chicago</h2>
+          <p className="hero-description">
+          We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.
+          </p>
+          <button className="reserve-button">Reserve a Table</button>
+        </div>
+
+        {/* Second Column - Image */}
+        <div className="hero-image">
+          <img
+            src="https://little-lemon-web.netlify.app/static/media/restauranfood.762bb670cf9db85c1b61.png"
+            alt="Restaurant"
+            className="hero-img"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import Hero from './Hero';
 
 function Homepage() {
   return (
    <div>
         <Header />
+        <Hero />
         <Footer />
    </div>
   );
